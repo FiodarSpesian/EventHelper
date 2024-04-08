0a0ba01a3b9c8def54760f75b3bee81284bfc8b1
Fiodar <fiodarspesian@mail.ru>
2024-04-08 15:52:25 +0300

    add model event, controller  events

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,26 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def search
+  end
+end
