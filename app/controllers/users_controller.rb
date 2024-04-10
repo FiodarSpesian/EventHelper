b320f3e33be1ad81795028ff3c46b51360bcb056
Fiodar <fiodarspesian@mail.ru>
2024-04-10 14:43:26 +0300

    add to user: REST[edit, update], view[edit]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,9 @@
 class UsersController < ApplicationController
   def new
-
     @user = User.new
   end
 
   def create
-    user_params = params.require(:user).permit(:name, :surname, :email, :password, :password_confirmation)
-
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
@@ -16,5 +13,28 @@ class UsersController < ApplicationController
       flash.now[:alert] = 'Вы неправильно заполнили поля формы регистрации'
       render :new
     end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      session[:user_id] = @user.id
+
+      redirect_to root_path, notice: 'Данные пользователя обновлены!'
+    else
+      flash.now[:alert] = 'При попытке сохранить пользователя возникли ошибки'
+      render :edit
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :surname, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: "events#index"
 
   resource :session, only: %i[new create destroy]
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create edit update]
   resources :events
 end
